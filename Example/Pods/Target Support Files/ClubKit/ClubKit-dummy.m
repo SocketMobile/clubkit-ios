a3d2e73278390d7c2921f373ea597e44249f934e
Chrishon <chrishon@socketmobile.com>
2020-05-18 10:54:07 -0400

    Initial commit

---
+++ Example/Pods/Target Support Files/ClubKit/ClubKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ClubKit : NSObject
+@end
+@implementation PodsDummy_ClubKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-ClubKit_Example/Pods-ClubKit_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ClubKit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ClubKit_Example
+@end
