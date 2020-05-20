8aec7e673b09baba7d11e65e33ab09fb11e72e5a
Chrishon <chrishon@socketmobile.com>
2020-05-19 20:47:24 -0400

    Refactored the onDecodedData function to return an optional Error and creating a static function for creating errors

--- Classes/CaptureMiddleware.swift
+++ Classes/CaptureMiddleware.swift
@@ -13,7 +13,9 @@ public class CaptureMiddleware: NSObject, CaptureMiddlewareProtocol {
     
     /// Accepts decoded data from a BLE device which can be used to
     /// manage users if the data is from a Mobile Pass
-    public func onDecodedData(decodedData: SKTCaptureDecodedData?, device: CaptureHelperDevice) {}
+    public func onDecodedData(decodedData: SKTCaptureDecodedData?, device: CaptureHelperDevice) -> Error? {
+        return nil
+    }
     
     /// Enum for different formats by which the decodedData will be parsed
     public enum DecodedDataParseFormat: Int {
@@ -27,5 +29,12 @@ public class CaptureMiddleware: NSObject, CaptureMiddlewareProtocol {
     /// Sets the format by which the decoded data will be parsed.
     public func setDecodedDataParse(format: DecodedDataParseFormat) {
         self.decodedDataFormat = format
+    }
+    
+    internal static func generateError(localizedString: String, comment: String? = nil, domain: String, code: Int? = nil) -> NSError {
+        let userInfo: [String: Any] = [
+            NSLocalizedDescriptionKey: NSLocalizedString(localizedString, comment: comment ?? "")
+        ]
+        return NSError(domain: domain, code: code ?? 0, userInfo: userInfo)
     }
 }
