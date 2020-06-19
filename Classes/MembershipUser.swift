0537fc95165bcbb3481a3b914574cd78ac42f8e2
Chrishon <chrishon@socketmobile.com>
2020-06-19 19:12:11 -0400

    Conformed the MembershipUser class to Codable and implemented necessary functions in preparation of sharing

--- Classes/MembershipUser.swift
+++ Classes/MembershipUser.swift
@@ -8,22 +8,22 @@
 import RealmSwift
 
 /// A default object which represents a user. May be subclassed
-open class MembershipUser: Object, IdentifiableUserProtocol, Identifiable {
+@objcMembers open class MembershipUser: Object, Codable, IdentifiableUserProtocol, Identifiable {
     
     /// Unique identifier for this user (Often supplied within the Mobile Pass)
-    @objc public dynamic var userId: String?
+    public dynamic var userId: String?
     
     /// Username for the user (Often supplied within the Mobile Pass)
-    @objc public dynamic var username: String?
+    public dynamic var username: String?
     
     /// Timestamp (in milliseconds since January 1, 1970) of this user's creation date
-    @objc public dynamic var timeStampAdded: Double = 0.0
+    public dynamic var timeStampAdded: Double = 0.0
     
     /// Number of times that the user has visited (or scanned their mobile pass)
-    @objc public dynamic var numVisits: Int = 0
+    public dynamic var numVisits: Int = 0
     
     /// Timestamp (in milliseconds since January 1, 1970) of the last visit
-    @objc public dynamic var timeStampOfLastVisit: Double = 0.0
+    public dynamic var timeStampOfLastVisit: Double = 0.0
     
     /// A RealmSwift - specific default initializer
     public required init() {
@@ -35,4 +35,50 @@ open class MembershipUser: Object, IdentifiableUserProtocol, Identifiable {
         return ClubConstants.RealmQueryConstants.userId
     }
     
+    /// Required initializer as part of conformance to Decodable protocol
+    /// If overriding the MembershipUser class, be sure to override this initializer, call `super.init(from:)`
+    /// and try to decode the properties in your subclass
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        userId = try container.decode(String.self, forKey: .userId)
+        username = try container.decode(String.self, forKey: .username)
+        timeStampAdded = try container.decode(Double.self, forKey: .timeStampAdded)
+        numVisits = try container.decode(Int.self, forKey: .numVisits)
+        timeStampOfLastVisit = try container.decode(Double.self, forKey: .timeStampOfLastVisit)
+        
+        super.init()
+    }
+    
+    /// Required as part of Encodable protocol.
+    /// If overriding the MembershipUser class, be sure to override this function, call `super.encode(to:)`
+    /// and try to encode the properties in your subclass
+    open func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(userId, forKey: .userId)
+        try container.encode(username, forKey: .username)
+        try container.encode(timeStampAdded, forKey: .timeStampAdded)
+        try container.encode(numVisits, forKey: .numVisits)
+        try container.encode(timeStampOfLastVisit, forKey: .timeStampOfLastVisit)
+        
+    }
+    
+    /// Returns array of string representation of MembershipUser variable names
+    /// If overriding the MembershipUser class, be sure to override this function and combine
+    /// the resulting array from the `super.variableNamesAsStrings()` and the variable
+    /// names in your names your subclass
+    /// Used for Codable protocol
+    open class func variableNamesAsStrings() -> [String] {
+        return CodingKeys.allCases.map { $0.rawValue }
+    }
+
+    /// enum for string representation of variable names. Used for Codable protocol
+    internal enum CodingKeys: String, CodingKey, CaseIterable {
+        case userId
+        case username
+        case timeStampAdded
+        case numVisits
+        case timeStampOfLastVisit
+    }
 }
---
+++ ExportableDataSourceContainer.swift
@@ -0,0 +1,8 @@
+//
+//  ExportableDataSourceContainer.swift
+//  ClubKit
+//
+//  Created by Chrishon Wyllie on 6/19/20.
+//
+
+import Foundation
