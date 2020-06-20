921066cf9e9f4990c5d3456b63f2c1c188f5a33e
Chrishon <chrishon@socketmobile.com>
2020-06-19 20:32:44 -0400

    Moved CustomMembershipUser to separate file

---
+++ Example/ClubKit/CustomMembershipUser.swift
@@ -0,0 +1,9 @@
+//
+//  CustomMembershipUser.swift
+//  ClubKit_Example
+//
+//  Created by Chrishon Wyllie on 6/19/20.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import Foundation
