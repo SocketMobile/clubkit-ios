df9bdec9f225533efcf6e418a1d1a6f215a782a4
Chrishon <chrishon@socketmobile.com>
2020-07-21 12:37:19 -0400

    Implemented function for parsing received CSV files

--- Classes/ExportableDataSourceContainer.swift
+++ Classes/ExportableDataSourceContainer.swift
@@ -11,7 +11,7 @@ internal class ExportableDataSourceContainer<T: MembershipUser>: Object, Codable
     
     internal private(set) var users: [T] = []
     
-    internal typealias MembershipUserDictionary = [String: AnyObject]
+    internal typealias MembershipUserDictionary = [String: Any]
     
     internal enum CodingKeys: String, CodingKey {
         case users
@@ -42,7 +42,7 @@ internal class ExportableDataSourceContainer<T: MembershipUser>: Object, Codable
             let allUsers = realm.objects(T.self)
             users = Array<T>(allUsers)
         } catch let error {
-            DebugLogger.shared.addDebugMessage("Error getting user: \(error)")
+            DebugLogger.shared.addDebugMessage("\(String(describing: type(of: self))) - Error getting user: \(error)")
         }
     }
     
@@ -120,7 +120,7 @@ internal class ExportableDataSourceContainer<T: MembershipUser>: Object, Codable
             for userObject: MembershipUserDictionary in allUsersAsJSONArray {
                 
                 let values: [String] = T.variableNamesAsStrings().map { (dictionaryKey) in
-                    let dictionaryValue: AnyObject? = userObject[dictionaryKey]
+                    let dictionaryValue = userObject[dictionaryKey]
                     return String(describing: dictionaryValue ?? "" as AnyObject)
                 }
                 
@@ -134,7 +134,7 @@ internal class ExportableDataSourceContainer<T: MembershipUser>: Object, Codable
             return filePath
             
         } catch let error {
-            DebugLogger.shared.addDebugMessage("ExportableDataSourceContainer - Error converting data source to CSV: \(error.localizedDescription)")
+            DebugLogger.shared.addDebugMessage("\(String(describing: type(of: self))) - Error converting data source to CSV: \(error.localizedDescription)")
             return nil
         }
     }
@@ -148,7 +148,7 @@ internal class ExportableDataSourceContainer<T: MembershipUser>: Object, Codable
             
             return result as? [MembershipUserDictionary]
         } catch {
-            DebugLogger.shared.addDebugMessage("ExportableDataSourceContainer - Error encoding all users as JSON array: \(error.localizedDescription)")
+            DebugLogger.shared.addDebugMessage("\(String(describing: type(of: self))) - Error encoding all users as JSON array: \(error.localizedDescription)")
             return nil
         }
     }
@@ -161,14 +161,14 @@ internal class ExportableDataSourceContainer<T: MembershipUser>: Object, Codable
         let documents: [URL] = FileManager.default.urls(for: FileManager.SearchPathDirectory.documentDirectory,
                                                         in: FileManager.SearchPathDomainMask.userDomainMask)
         
-        guard let fileURL = documents.first else {
-            DebugLogger.shared.addDebugMessage("ExportableDataSourceContainer - Error creating file URL")
+        guard let documentsURL = documents.first else {
+            DebugLogger.shared.addDebugMessage("\(String(describing: type(of: self))) - Error creating file URL")
             return nil
         }
         
         let pathComponent: String = "/\(fileName).\(fileExtension)"
         
-        let filePath: URL = fileURL.appendingPathComponent(pathComponent)
+        let filePath: URL = documentsURL.appendingPathComponent(pathComponent)
         
         return filePath
     }
@@ -184,6 +184,8 @@ internal class ExportableDataSourceContainer<T: MembershipUser>: Object, Codable
         switch url.pathExtension {
         case IOFileType.userList.fileExtension:
             return parseImportedUserListFile(at: url)
+        case IOFileType.csv.fileExtension:
+            return parseImportedUserCSVFile(at: url)
         default: return nil
         }
     }
@@ -198,7 +200,85 @@ internal class ExportableDataSourceContainer<T: MembershipUser>: Object, Codable
             return users
             
         } catch let error {
-            print(error.localizedDescription)
+            DebugLogger.shared.addDebugMessage("\(String(describing: type(of: self))) - Error converting data in URL to Users. Error: \(error)")
+            return nil
+        }
+    }
+    
+    private static func parseImportedUserCSVFile(at url: URL) -> [T]? {
+        guard let csvRows = getCSVRows(from: url) else {
+            return nil
+        }
+        
+        guard let columnNames: [String] = csvRows.first?.components(separatedBy: ",") else {
+            return nil
+        }
+        
+        let userRows: [String] = Array<String>(csvRows[1..<csvRows.count])
+        
+        let arrayOfDictionaries: [MembershipUserDictionary] = convertToDictionaryArray(csvLines: userRows, columnNames: columnNames)
+        
+        guard let contentsAsJsonString: String = convertToJSONString(array: arrayOfDictionaries) else {
+            return nil
+        }
+        
+        try? FileManager.default.removeItem(at: url)
+        
+        return getUsersFrom(jsonString: contentsAsJsonString)
+    }
+    
+    private static func getCSVRows(from url: URL) -> [String]? {
+        do {
+            let contents = try String(contentsOf: url, encoding: String.Encoding.utf8)
+            
+            let csvRows: [String] = contents.components(separatedBy: .newlines)
+            
+            return csvRows
+        } catch let error {
+            DebugLogger.shared.addDebugMessage("\(String(describing: type(of: self))) - Error getting CSV rows from URL. Error: \(error)")
+            return nil
+        }
+    }
+    
+    private static func convertToDictionaryArray(csvLines: [String], columnNames: [String]) -> Array<MembershipUserDictionary> {
+        var result = Array<MembershipUserDictionary>()
+        
+        for line in csvLines {
+            let fieldValues = line.components(separatedBy: ",")
+            // NOTE
+            // This assumes all the field values (columns) are Strings
+            // This also assumes a sort of "generic" format
+            // If your data contains Integers, Floats, etc, you should
+            // parse them individually
+            // For example, if you know the third column is an integer:
+            // let value = Int(fieldValues[2])
+            // Or, alternatively, when decoding User objects, cast the decoded String
+            // back to an integer
+            let dictionary = Dictionary(zip(columnNames, fieldValues), uniquingKeysWith: { (first, _) in first })
+            result.append(dictionary)
+        }
+        return result
+    }
+    
+    private static func convertToJSONString(array: [MembershipUserDictionary]) -> String? {
+        do {
+            let jsonData: Data = try JSONSerialization.data(withJSONObject: array, options: .prettyPrinted)
+            return String(data: jsonData, encoding: String.Encoding.utf8)
+        } catch let error {
+            DebugLogger.shared.addDebugMessage("\(String(describing: type(of: self))) - Error converting Users to JSON string. Error: \(error)")
+            return nil
+        }
+    }
+    
+    private static func getUsersFrom(jsonString: String) -> [T]? {
+        guard let importedDataSourceAsData: Data = jsonString.data(using: String.Encoding.utf8) else {
+            return nil
+        }
+        do {
+            let users = try JSONDecoder().decode([T].self, from: importedDataSourceAsData)
+            return users
+        } catch let error {
+            DebugLogger.shared.addDebugMessage("\(String(describing: type(of: self))) - Error converting JSON data to Users. Error: \(error)")
             return nil
         }
     }
