ea3e376375358228f90573cb0fc2f9b91f300866
Chrishon <chrishon@socketmobile.com>
2020-09-17 12:49:47 -0400

    Changes reflecting version migrations

--- Example/ClubKit/CustomMembershipUser.swift
+++ Example/ClubKit/CustomMembershipUser.swift
@@ -11,6 +11,13 @@ import ClubKit
 @objcMembers class CustomMembershipUser: MembershipUser {
     
     dynamic var emailAddress: String? = "Some Email address"
+    dynamic var greeting: String?
+    dynamic var age: Int = 0
+    dynamic var field1: Int = 1
+    dynamic var field2: Int = 2
+    dynamic var countryCode: String = ""
+//    dynamic var field4: Int = 4
+//    dynamic var field5: Int = 5
     
     override class func variableNamesAsStrings() -> [String] {
         let superclassNames = super.variableNamesAsStrings()
@@ -19,6 +26,13 @@ import ClubKit
 
     enum CodingKeys: String, CodingKey, CaseIterable {
         case emailAddress
+        case greeting
+        case age
+        case field1
+        case field2
+        case countryCode // created as field3 (before field4) then renamed to countryCode (after deleting field4)
+        case field4 // created (before field5) then deleted (after deleting field5)
+        case field5 // created then deleted
     }
     
     required init() {
@@ -29,6 +43,13 @@ import ClubKit
         try super.init(from: decoder)
         let container = try decoder.container(keyedBy: CodingKeys.self)
         emailAddress = try container.decode(String.self, forKey: .emailAddress)
+        greeting = try container.decode(String.self, forKey: .greeting)
+        age = try container.decode(Int.self, forKey: .age)
+        field1 = try container.decode(Int.self, forKey: .field1)
+        field2 = try container.decode(Int.self, forKey: .field2)
+        countryCode = try container.decode(String.self, forKey: .countryCode)
+//        field4 = try container.decode(Int.self, forKey: .field4)
+//        field5 = try container.decode(Int.self, forKey: .field5)
     }
     
     public override func encode(to encoder: Encoder) throws {
@@ -36,5 +57,12 @@ import ClubKit
         var container = encoder.container(keyedBy: CodingKeys.self)
         
         try container.encode(emailAddress, forKey: .emailAddress)
+        try container.encode(greeting, forKey: .greeting)
+        try container.encode(age, forKey: .age)
+        try container.encode(field1, forKey: .field1)
+        try container.encode(field2, forKey: .field2)
+        try container.encode(countryCode, forKey: .countryCode)
+//        try container.encode(field4, forKey: .field4)
+//        try container.encode(field5, forKey: .field5)
     }
 }
